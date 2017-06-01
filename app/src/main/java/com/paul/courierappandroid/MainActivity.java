e1c63864864f6b0f8407010d5fc59618b1a308e0
Pawel <pa7wel@gmail.com>
2017-06-01 21:31:58 +0200

    Update route after click checkbox.

--- app/src/main/java/com/paul/courierappandroid/MainActivity.java
+++ app/src/main/java/com/paul/courierappandroid/MainActivity.java
@@ -22,4 +22,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, RouteActivity.class);
         startActivity(intent);
     }
+
+    public void locationPage(View view) {
+        Intent intent = new Intent(this, LocationActivity.class);
+        startActivity(intent);
+    }
 }
